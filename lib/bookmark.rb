318184686f1c757ad1c62a6349619292b26d1277
Christian Pinna <christian.a.pinna@gmail.com>
2022-03-15 14:55:57 +0000

    refactored bookmark.all to use a .map method instead of a .each method

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -3,13 +3,10 @@ require 'pg'
 class Bookmark 
 
   def self.all
-    urls = []
     connection = PG.connect( dbname: 'bookmark_manager' )
     results = connection.exec('SELECT url FROM bookmarks')
-    results.each do |row| 
-      urls << row['url']
+    results.map do |row| row['url']
     end
-    urls
   end 
   
 end
